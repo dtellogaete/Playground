f0520d9b54c1276eef8af441bff1e4f52f428c39
Daniel Alexander Tello Gaete <dtellogaete@gmail.com>
2019-04-14 23:48:56 -0400

    14/04

--- webplayground/webplayground/settings.py
+++ webplayground/webplayground/settings.py
@@ -31,13 +31,16 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'registration',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'ckeditor',
     'core',
+    'pages.apps.PagesConfig',
 ]
 
 MIDDLEWARE = [
@@ -119,3 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
--- webplayground/webplayground/urls.py
+++ webplayground/webplayground/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from pages.urls import pages_patterns
 
 urlpatterns = [
     path('', include('core.urls')),
+    path('pages/', include(pages_patterns)),
     path('admin/', admin.site.urls),
+    #Paths de Auth
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('registration.urls')),
 ]
